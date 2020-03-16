7e1bb5b970ab2c1697d5fe9a7011365c78eb4c18
andrew_marhai <buk2018irinam@gmail.com>
2020-03-16 13:54:04 +0300

    Погода в Брянске - фронт

--- resources/assets/js/components/site/Weather.vue
+++ resources/assets/js/components/site/Weather.vue
@@ -1,30 +1,50 @@
 <template>
     <div class="container">
         <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-            <h1 class="display-4">Погода в Брянске в реальном времени</h1>
-            <p class="lead">
-                Тестовый параграф
+            <h2 class="display-6">Погода в Брянске</h2>
+            <p v-if="weatherData.icon_uri" class="lead">
+                <img :src="weatherData.icon_uri" alt="weather_icon">
+            </p>
+            <p v-if="weatherData.current_temp" class="lead">
+                Температура: {{ weatherData.current_temp }}°C
+            </p>
+            <p v-if="weatherData.current_temp" class="lead">
+                Ощущается: {{ weatherData.feels_like_temp }}°C
+            </p>
+            <p v-if="weatherData.pressure" class="lead">
+                Давление: {{ weatherData.pressure }} мм рт. ст.
             </p>
         </div>
     </div>
 </template>
 
 <script>
+    const cityName = 'bryansk';
+    // коэффициент для перевода давления из паскалей в ммрс
+    const mmhgСoefficient = 0.750062;
     export default {
         mounted() {
-            this.getUsersList();
+            this.getWeatherByCityName();
         },
         data() {
             return {
-                usersList: {},
+                weatherData: {
+                    icon_uri: null,
+                    current_temp: null,
+                    feels_like_temp: null,
+                    pressure: null,
+                },
             }
         },
         methods: {
-            getUsersList(page = 1) {
-                this.axios.get('/' + page)
+            getWeatherByCityName() {
+                this.axios.get(`/weather/get-by-city/${cityName}`)
                     .then(response => {
                         console.log(response.data);
-                        this.usersList = response.data;
+                        this.weatherData.icon_uri = response.data.icon_uri;
+                        this.weatherData.current_temp = response.data.current_temp;
+                        this.weatherData.feels_like_temp = response.data.feels_like_temp;
+                        this.weatherData.pressure = Math.round(response.data.pressure * mmhgСoefficient);
                     });
             }
         }
